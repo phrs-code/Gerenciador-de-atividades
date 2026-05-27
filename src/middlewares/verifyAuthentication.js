a271a36905830e5c148aeecf429eb1837d71959f
Pedro Henrique Rodrigues da Silva <pedrohrs@motorola.com>
2026-05-27 18:57:49 -0300

    middleares / verificação de tokens

--- src/middlewares/verifyAuthentication.js
+++ src/middlewares/verifyAuthentication.js
@@ -1,1 +1,42 @@
 const jwt = require('jsonwebtoken');
+
+const verifyAuthenticate = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if(!authorization) {
+        return res.status(401).json({
+            error: "@authorization/missing-token",
+            message: "Acesso negado (Token de autenticação)"
+        });
+    };
+
+    const [prefix, token] = authorization.split(' ');
+
+    const invalidToken = {
+            error: "@authorization/invalid-token",
+            message: "Token provided is invalid"
+        }
+
+    if (prefix !== "Bearer") {
+        return res.status(401).json(invalidToken);
+    };
+
+    if (!token) {
+        return res.status(401).json(invalidToken);
+    };
+
+    jwt.verify(token, "secret", (err, decoded) => {
+        if (err) {
+            return res.status(401).json(invalidToken);
+        }
+
+        req.user = decoded;
+
+        return next();
+    });
+
+};
+
+module.exports = {
+    verifyAuthenticate,
+};
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -3,10 +3,11 @@ const { Router } = require('express');
 const routes = Router();
 
 const tasksControllers = require('../controllers/tasks.controllers')
+const { verifyAuthenticate } = require('../middlewares/verifyAuthentication')
 
 routes.get('/tasks', tasksControllers.list);
 
-routes.post('/tasks', tasksControllers.create);
+routes.post('/tasks', verifyAuthenticate, tasksControllers.create);
 
 routes.get('/tasks/:id', tasksControllers.getById);
 
