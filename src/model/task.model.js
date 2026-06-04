fda0707666fb3ec2b3bdbbc22ead99cba06b15a8
Pedro Henrique Rodrigues da Silva <pedrohrs@motorola.com>
2026-06-04 13:22:03 -0300

    mudanca de ordem

--- src/model/task.model.js
+++ src/model/task.model.js
@@ -9,16 +9,16 @@ const TaskSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    prioridade: {
-        type: String,
-        enum: ['baixa', 'média', 'alta'],
-        default: 'média',
-        required: true
-    },
     status: {
         type: String,
         enum: ['não iniciado', 'em andamento', 'concluído'],
         default: 'não iniciado',
+        required: true
+    },
+    prioridade: {
+        type: String,
+        enum: ['baixa', 'média', 'alta'],
+        default: 'média',
         required: true
     },
     userId: {
